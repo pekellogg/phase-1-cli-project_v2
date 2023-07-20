68dc181eff7053bdb473a70d42e93450f57902c4
Peyton Kellogg <peytonkellogg@gmail.com>
2023-07-19 21:14:13 -0700

    adds code comments; NOTE: API calls have hard-coded dates that are out of date and result in unhandled http response errors

--- data/api_calls.rb
+++ data/api_calls.rb
@@ -1,7 +1,10 @@
+require 'pry'
+
 class APICall
     BASE = "https://api.propublica.org/congress/v1"
-    PROPUBLICA = ENV.fetch("legislators_key")
+    PROPUBLICA = ENV['LEGISLATORS_KEY']
 
+    # https://api.propublica.org/congress/v1/117/senate/members.json
     #usage: Legislator class-level for Legislator.all
     def self.get_legislators(uri)
         response = RestClient.get(uri, headers = {"X-API-Key": PROPUBLICA})
@@ -9,6 +12,7 @@ class APICall
         body["results"][0]["members"]
     end
 
+    # https://api.propublica.org/congress/v1/117/house/members.json
     #usage: Vote class-level for Vote.all, past 30 days
     def self.get_votes(uri)
         response = RestClient.get(uri, headers = {"X-API-Key": PROPUBLICA})
@@ -16,6 +20,7 @@ class APICall
         body["results"]["votes"]
     end
 
+    # https://api.propublica.org/congress/v1/both/votes/2023-06-19/2023-07-19.json
     #usage: vote instance-level positions
     def self.get_vote_positions(uri)
         response = RestClient.get(uri, headers = {"X-API-Key": PROPUBLICA})
